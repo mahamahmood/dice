79bc4342772d48f93e19f6be3e94a977f9cbf3cc
Maha Mahmood <>
2020-12-14 13:03:06 -0500

    created a die component. added fontawesome cdn to public/index.html. Die compoenets are showing in app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Die from './Die';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Die face='five' />
+        <Die face='six' />
+        <Die face='three' />
+        <Die face='two' />
+      </div>
+    );
+  }
 }
 
 export default App;
